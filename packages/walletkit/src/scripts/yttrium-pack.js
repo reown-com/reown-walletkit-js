ce955763a7ca54ccdb6c0f21e815f4946fc2c024
Gancho Radkov <ganchoradkov93@gmail.com>
2025-01-23 11:48:22 +0200

    chore: prettier of pack script

--- packages/walletkit/src/scripts/yttrium-pack.js
+++ packages/walletkit/src/scripts/yttrium-pack.js
@@ -4,21 +4,21 @@ const path = require("path");
 const { compress } = require("brotli");
 
 function compressWasmAndConvertToBase64(wasmFilePath, outputTsFilePath) {
-    try {
-        const wasmBuffer = fs.readFileSync(wasmFilePath);
+  try {
+    const wasmBuffer = fs.readFileSync(wasmFilePath);
 
-        const compressedWasm = compress(wasmBuffer);
+    const compressedWasm = compress(wasmBuffer);
 
-        const base64Wasm = Buffer.from(compressedWasm).toString("base64");
+    const base64Wasm = Buffer.from(compressedWasm).toString("base64");
 
-        const tsContent = `export const yttrium = "${base64Wasm}";\n`;
+    const tsContent = `export const yttrium = "${base64Wasm}";\n`;
 
-        fs.writeFileSync(outputTsFilePath, tsContent);
+    fs.writeFileSync(outputTsFilePath, tsContent);
 
-        console.log(`Compressed and saved Base64 wasm to ${outputTsFilePath}`);
-    } catch (error) {
-        console.error("Error during compression:", error);
-    }
+    console.log(`Compressed and saved Base64 wasm to ${outputTsFilePath}`);
+  } catch (error) {
+    console.error("Error during compression:", error);
+  }
 }
 
 const wasmFilePath = path.resolve(__dirname, "./../libs/yttrium/yttrium_bg.wasm");
