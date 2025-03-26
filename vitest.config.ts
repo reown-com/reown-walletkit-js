1d13d6c318f1fdba9d0707299e0a356b22b15502
Gancho Radkov <ganchoradkov93@gmail.com>
2025-03-26 15:44:53 +0200

    chore: 1.2.3 release

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   define: {
     "process.env.IS_VITEST": true,
+    "process.env.DISABLE_GLOBAL_CORE": true,
   },
   test: {
     testTimeout: 800_000,
